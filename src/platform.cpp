9187bb14882e179d57d8093579aaf5be26bc7a73
viktormajzus <60273278+viktormajzus@users.noreply.github.com>
2025-04-17 14:55:59 +0200

    Minor change to platform.cpp, removed redundant namespace

--- src/platform.cpp
+++ src/platform.cpp
@@ -6,20 +6,17 @@
 
 namespace fs = std::filesystem;
 
-namespace file
+std::expected<std::filesystem::path, Error::Type> file::getDefaultPath()
 {
-  std::expected<std::filesystem::path, Error::Type> file::getDefaultPath()
-  {
 #ifdef LOCKFORGE_PLATFORM_WINDOWS
-    const char* p_appdata{ std::getenv("APPDATA") };
-    if (!p_appdata)
-      return std::unexpected(Error::Type::EnvMissing);
-    return fs::path(p_appdata) / "LockForge" / "vault.lf";
+  const char* p_appdata{ std::getenv("APPDATA") };
+  if (!p_appdata)
+    return std::unexpected(Error::Type::EnvMissing);
+  return fs::path(p_appdata) / "LockForge" / "vault.lf";
 #else
-    const char* home{ std::getenv("HOME") };
-    if (!home)
-      return std::unexpected(Error::Type::EnvMissing);
-    return fs::path(home) / ".lockforge" / "vault.lf";
+  const char* home{ std::getenv("HOME") };
+  if (!home)
+    return std::unexpected(Error::Type::EnvMissing);
+  return fs::path(home) / ".lockforge" / "vault.lf";
 #endif
-  }
 }
